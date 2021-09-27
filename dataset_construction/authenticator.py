fed8aa1ee8462bbb0369c6930ae2e38c7d13ac5e
imdaredevil <cibi_16@live.com>
2021-09-26 19:25:46 -0700

    setting up file to store tokens

--- dataset_construction/authenticator.py
+++ dataset_construction/authenticator.py
@@ -2,7 +2,7 @@ import os
 import requests
 from requests.api import head 
 from requests.auth import HTTPBasicAuth
-import time
+from datetime import timedelta, datetime
 import sys
 import traceback
 
@@ -11,17 +11,26 @@ SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
 
 URL = "https://accounts.spotify.com/api/token"
 GRANT_TYPE = "client_credentials"
+TOKEN_PATH = "../credentials/client_credentials_token"
 
 
 class Authenticator:
 
     def __init__(self):
-        self.token = None
-        self.expiry = None
+        if os.path.exists(TOKEN_PATH):
+            token_file = open(TOKEN_PATH,'r')
+            self.token, self.expiry = token_file.read().split('\n')
+            self.expiry = datetime.fromisoformat(self.expiry)
+            token_file.close()
+        else:
+            self.token = None
+            self.expiry = None
     
     def get_auth_token(self):
-        if (not self.expiry) or time.time() > self.expiry:
+        if (not self.expiry) or datetime.now() > self.expiry:
          self.token,self.expiry = self._get_auth_token()
+         token_file = open(TOKEN_PATH, 'w')
+         token_file.write(self.token + '\n' + str(self.expiry))
         return self.token
 
     def _get_auth_token(self):
@@ -33,7 +42,7 @@ class Authenticator:
                 )
             print(response.json())
             response_dict = response.json()
-            return (response_dict["access_token"], time.time() + response_dict['expires_in'])
+            return (response_dict["access_token"], datetime.now() + timedelta(0,response_dict['expires_in']))
         except:
             print(sys.exc_info()[2])
             print(traceback.format_exc())
