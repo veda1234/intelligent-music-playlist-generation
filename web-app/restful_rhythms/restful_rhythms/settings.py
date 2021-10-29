8f33cc1c33a9de723c2d170151ff6a272c734208
imdaredevil <cibi_16@live.com>
2021-10-28 18:33:27 -0700

    removing rest_framework

--- web-app/restful_rhythms/restful_rhythms/settings.py
+++ web-app/restful_rhythms/restful_rhythms/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'songs.apps.SongsConfig',
-    'rest_framework',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
