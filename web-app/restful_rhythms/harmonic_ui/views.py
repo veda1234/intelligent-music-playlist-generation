0de542b2a69e9bf62a84a22b4897ba9b399935cf
imdaredevil <cibi_16@live.com>
2021-10-28 19:46:27 -0700

    fixing authentication

--- web-app/restful_rhythms/harmonic_ui/views.py
+++ web-app/restful_rhythms/harmonic_ui/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from spotify.util import is_spotify_authenticated
 
 # Create your views here.
 
 
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
+
+def index_with_authentication(request, *args, **kwargs):
+    if is_spotify_authenticated(request.session.session_key):
+        return render(request, 'frontend/index.html')
+    return redirect('/')
