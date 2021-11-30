3eee30ddc26b16072777d91292dabd8dac7b969b
imdaredevil <cibi_16@live.com>
2021-11-29 17:32:31 -0800

    adding artist and album url to routes

--- web-app/restful_rhythms/harmonic_ui/urls.py
+++ web-app/restful_rhythms/harmonic_ui/urls.py
@@ -5,5 +5,7 @@ app_name='harmonic_ui'
 
 urlpatterns = [
     path('',index,name=''),
-    path('welcome', index_with_authentication,name='welcome')
+    path('welcome', index_with_authentication,name='welcome'),
+    path('artists', index_with_authentication,name='artists'),
+    path('albums', index_with_authentication,name='albums')    
 ]
