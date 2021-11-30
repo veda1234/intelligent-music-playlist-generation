c1fab4cfef9e3c03655b57a837986b9620030210
imdaredevil <cibi_16@live.com>
2021-11-29 23:48:53 -0800

    new table creation

---
+++ dataset_construction/firebase_artist_uploader.py
@@ -0,0 +1,32 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import pickle
+import json
+import os
+
+# Use a service account
+cred = credentials.Certificate('../credentials/firestore-key.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+if __name__ == '__main__':
+    artist_ref = db.collection('artists')
+    track_ref = db.collection('tracks')
+    i = 0
+    r = None
+    try:
+        for record in db.collection('tracks').order_by('id').start_at({ 'id': '0NeJjNlprGfZpeX2LQuN6c' }).stream():            
+            r  = record.to_dict()
+            for artist in r['artists']:
+                artist_ref.document(artist['id']).set(artist)
+            i += 1
+            track_ref.document(r['id']).update({ 'artist_id': [artist['id'] for artist in r['artists']] })
+            if i % 100 == 99:
+                print(f'{i+1} done')
+    except:
+        print(f'error at {r}')
+        raise
+
+    
